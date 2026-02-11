bee05d9e6169f4efdd47a1b124ce15ab24957d2f
Shouvik-1999 <134383695+Shouvik-1999@users.noreply.github.com>
2026-02-11 22:49:35 +0530

    Fix typo in greeting string for consistency

--- src/com/java/practice/StringRev.java
+++ src/com/java/practice/StringRev.java
@@ -10,7 +10,7 @@ public class StringRev {
     }
     public static void main(String[] args) {
         StringRev obj = new StringRev();
-        String str = "Hello, New Programmers!";
+        String str = "Hello, New Programmer!";
         String reversedStr = obj.reverseString(str);
         System.out.println("Original String: " + str);
         System.out.println("Reversed String: " + reversedStr);
