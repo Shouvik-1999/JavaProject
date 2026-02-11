334c19900ea742f0836da4feb4c430c1934567a6
Shouvik-1999 <134383695+Shouvik-1999@users.noreply.github.com>
2026-02-11 22:46:04 +0530

    Change last element of array from 5 to 8

--- src/com/java/practice/IntSum.java
+++ src/com/java/practice/IntSum.java
@@ -2,7 +2,7 @@ package com.java.practice;
 
 public class IntSum {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 8};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
